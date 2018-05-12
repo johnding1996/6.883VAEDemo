7377f49f9647f452feb6c29c2e54154c2126a66e
mcding <mcding@mit.edu>
2018-05-12 16:48:14 -0400

    finished the basic demo page

--- util.js
+++
@@ -1,5 +0,0 @@
-function grayscale2color(percentage) {
-    var color_part_dec = 255 * percentage;
-    var color_part_hex = Number(parseInt( color_part_dec , 10)).toString(16);
-    return "#" + color_part_hex + color_part_hex + color_part_hex;
-}
